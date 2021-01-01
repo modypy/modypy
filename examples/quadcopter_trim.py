c6d31a7d3c2a5eafb14f482236a4057168828c72
Ralf Gerlich <ralf@ralfgerlich.biz>
2021-01-01 13:32:05 +0100

    Add aerodynamics block and quadrocopter trim example.

---
+++ examples/quadcopter_trim.py
@@ -0,0 +1,145 @@
+import itertools
+
+import numpy as np
+import scipy.linalg as la
+
+from simtree.blocks import NonLeafBlock
+from simtree.blocks.aerodyn import Propeller, Thruster
+from simtree.blocks.elmech import DCMotor
+from simtree.blocks.sources import Constant
+from simtree.blocks.linear import Sum
+from simtree.compiler import Compiler
+from simtree.linearization import find_steady_state, system_jacobian
+
+
+class Engine(NonLeafBlock):
+    def __init__(self, ct, cp, D, Kv, R, L, J, direction, vector, arm, **kwargs):
+        NonLeafBlock.__init__(self,
+                              num_inputs=2,
+                              num_outputs=6,
+                              **kwargs)
+        self.dcmotor = DCMotor(Kv, R, L, J, name="dcmotor")
+        self.propeller = Propeller(thrust_coeff=ct,
+                                   power_coeff=cp,
+                                   diameter=D,
+                                   name="propeller")
+        self.thruster = Thruster(direction=direction,
+                                 vector=vector,
+                                 arm=arm)
+        # Add the children
+        self.add_block(self.dcmotor)
+        self.add_block(self.propeller)
+        self.add_block(self.thruster)
+        # Connect the input voltage to the motor
+        self.connect_input(0, self.dcmotor, 0)
+        # Connect the torque of the propeller to the motor
+        self.connect(self.propeller, 1, self.dcmotor, 1)
+        # Connect the speed of the motor to the speed of the propeller
+        self.connect(self.dcmotor, 0, self.propeller, 0)
+        # Connect the input density to the density for the propeller
+        self.connect_input(1, self.propeller, 1)
+        # Connect the thrust of the propeller to the thruster
+        self.connect(self.propeller, 0, self.thruster, 0)
+        # Connect the torque of the DC motor to the thruster
+        self.connect(self.dcmotor, 2, self.thruster, 1)
+        # Connect the thrust and torque of the thruster to outputs 0:6
+        self.connect_output(self.thruster, range(6), range(6))
+
+parameters = {
+    'Kv': 789.E-6,
+    'R': 43.3E-3,
+    'L': 1.9E-3,
+    'J': 5.284E-6,
+    'ct': 0.09,
+    'cp': 0.04,
+    'D': 8*25.4E-3
+}
+
+rx = 0.25
+ry = 0.25
+
+engines = [
+    Engine(name="engine1",
+           vector=np.c_[0, 0, -1], arm=np.c_[+rx, +ry, 0], direction=1,
+           **parameters),
+    Engine(name="engine2",
+           vector=np.c_[0, 0, -1], arm=np.c_[+rx, -ry, 0], direction=-1,
+           **parameters),
+    Engine(name="engine3",
+           vector=np.c_[0, 0, -1], arm=np.c_[-rx, -ry, 0], direction=1,
+           **parameters),
+    Engine(name="engine4",
+           vector=np.c_[0, 0, -1], arm=np.c_[-rx, +ry, 0], direction=-1,
+           **parameters),
+]
+
+gravity_source = Constant(value=np.c_[0, 0, 1.5*9.81], name="gravity")
+density = Constant(value=1.29, name="rho")
+thrust_sum = Sum(channel_weights=[1, 1, 1, 1, 1], channel_dim=3, name="thrust")
+torque_sum = Sum(channel_weights=[1, 1, 1, 1], channel_dim=3, name="torque")
+
+frame = NonLeafBlock(name="frame",
+                     num_inputs=4,
+                     num_outputs=6)
+frame.add_block(thrust_sum)
+frame.add_block(torque_sum)
+frame.add_block(gravity_source)
+frame.add_block(density)
+for idx, engine in zip(itertools.count(), engines):
+    # Add each of the engines and connect it
+    frame.add_block(engine)
+    # Connect the voltage input
+    frame.connect_input(idx, engine, 0)
+    # Connect the density
+    frame.connect(density, 0, engine, 1)
+    # Connect the thrust output to the thrust sum
+    frame.connect(engine, range(3), thrust_sum, range(3*idx, 3*idx+3))
+    # Connect the torque output to the thrust sum
+    frame.connect(engine, range(3, 6), torque_sum, range(3*idx, 3*idx+3))
+# Connect the gravity to the thrust sum
+frame.connect(gravity_source, range(3), thrust_sum, range(3*len(engines), 3*len(engines)+3))
+# Connect the thrust and torque sums to the outputs
+frame.connect_output(thrust_sum, range(3), range(3))
+frame.connect_output(torque_sum, range(3), range(3, 6))
+
+# Compile the frame
+compiler = Compiler(frame)
+compiled_system = compiler.compile()
+
+# solution: omega=855, i=66.77, v=3.565
+
+omega0, i0, v0 = 1, 0, 0
+
+x_start = 4 * [omega0, i0]
+u_start = 4 * [v0]
+
+# Find the steady state of the system
+sol, x0, u0 = find_steady_state(compiled_system,
+                                time=0,
+                                x_start=x_start,
+                                u_start=u_start,
+                                solver_options={
+                                    'maxiter': 500*(12+1)
+                                })
+dxdt0 = compiled_system.state_update_function(0, x0, u0)
+y0 = compiled_system.output_function(0, x0, u0)
+A, B, C, D = system_jacobian(compiled_system, 0, x0, u0)
+
+print("success=%s nfev=%d" % (sol.success, sol.nfev))
+if sol.success:
+    print("\tx0  =%s" % x0)
+    print("\tu0  =%s" % u0)
+    print("\tdxdt=%s" % dxdt0)
+    print("\ty0  =%s" % y0)
+    print("A:")
+    print(A)
+    print("B:")
+    print(B)
+    print("C:")
+    print(C)
+    print("D:")
+    print(D)
+
+    print(la.eig(A))
+else:
+    print("message=%s" % sol.message)
---
+++ simtree/blocks/aerodyn.py
@@ -0,0 +1,125 @@
+"""
+A collection of blocks useful for aerodynamics.
+"""
+import math
+
+import numpy as np
+
+from simtree.blocks import LeafBlock
+
+
+class Propeller(LeafBlock):
+    """
+    A block representing a propeller.
+
+    The block has two inputs:
+        - The speed of the propeller n, and
+        - the density of the air rho.
+
+    It provides three outputs:
+        - The thrust vector F,
+        - the braking torque tau,
+        - the total braking power P.
+
+    The magnitudes of thrust, torque and power are determined by the following
+    formulae:
+
+        F = ct(n)*rho*D^5*n^2
+        tau = cp(n)/(2*pi)*rho*D^5*n^2
+        P = cp(n)*rho*D^5*n^3
+
+    Here,
+        - ct(n) is the thrust coefficient at speed n,
+        - cp(n) is the power coefficient at speed n, and
+        - D is the diameter.
+    """
+
+    def __init__(self,
+                 thrust_coeff,
+                 power_coeff,
+                 diameter,
+                 **kwargs):
+        LeafBlock.__init__(self, num_inputs=2, num_outputs=3, **kwargs)
+        if not callable(thrust_coeff):
+            thrust_coeff_value = thrust_coeff
+            thrust_coeff = (lambda n: thrust_coeff_value)
+        if not callable(power_coeff):
+            power_coeff_value = power_coeff
+            power_coeff = (lambda n: power_coeff_value)
+
+        self.thrust_coeff = thrust_coeff
+        self.power_coeff = power_coeff
+        self.diameter = diameter
+
+    def output_function(self, t, inputs):
+        n = inputs[0]
+        rho = inputs[1]
+
+        thrust = self.thrust_coeff(n) * rho * self.diameter**4 * n**2
+        power = self.power_coeff(n) * rho * self.diameter**5 * n**3
+        torque = np.sign(n) * power/(2*math.pi*n)
+
+        return np.c_[thrust, torque, power]
+
+
+class Thruster(LeafBlock):
+    """
+    A block representing a thruster.
+
+    A thruster converts scalar thrust and torque to vectorial thrust and torque.
+    The thrust is considered to work in a (constant) thrust direction, determined
+    by the thrust axis.
+
+    Torque is combined thrust along the thrust axis and torque due to the thrust
+    working at the end of a (constant) arm relative to the center of gravity
+    (CoG) of a rigid body.
+
+    The block has two inputs:
+        - The scalar thrust in the direction of the thrust axis, and
+        - the scalar torque along the thrust axis.
+
+    The block has six outputs:
+        - The thrust vector (3 components), and
+        - the torque vector (3 components).
+
+    It is configured by
+        - the thrust axis (vector X,Y,Z),
+        - the thrust arm (vector X,Y,Z), and
+        - the turning direction (scalar: +1 or -1).
+
+    The right-hand rule applied to the thrust axis gives the positive direction
+    of the thruster. Torque acts in the *opposite* direction, i.e. if the
+    thruster turns clockwise, torque acts counter-clockwise.
+    """
+    def __init__(self,
+                 vector=None,
+                 arm=None,
+                 direction=1,
+                 **kwargs):
+        LeafBlock.__init__(self,
+                           num_inputs=2,
+                           num_outputs=6,
+                           **kwargs)
+        if vector is None:
+            vector = np.c_[0, 0, -1]
+        if arm is None:
+            arm = np.zeros(3)
+
+        self.vector = vector
+        self.arm = arm
+        self.direction = direction
+
+    def output_function(self, t, inputs):
+        del t  # unused
+
+        thrust = inputs[0]
+        torque = inputs[1]
+
+        thrust_vector = self.vector * thrust
+        torque_vector = self.direction * self.vector * torque + \
+            np.cross(self.arm, thrust_vector)
+
+        return np.concatenate(
+            (thrust_vector, torque_vector),
+            axis=None
+        )
