3346ee8ab3e0e25eee9a2c2dab23e340138fbdc2
Ralf Gerlich <ralf@ralfgerlich.biz>
2021-05-30 20:31:31 +0200

    Introduce versioneer for version deduction.

--- modypy/__init__.py
+++ modypy/__init__.py
@@ -1,1 +1,7 @@
-from .version import __version__
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,49 +1,7 @@
 import setuptools
-
-# get the version
-with open("modypy/version.py") as fp:
-    exec(fp.read())
-
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import versioneer
 
 setuptools.setup(
-    name="modypy",
-    version_format=__version__,
-    author="Ralf Gerlich",
-    author_email="ralf@ralfgerlich.biz",
-    description="A framework for hierarchical modelling and simulation of "
-                "dynamic systems",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/ralfgerlich/modypy",
-    license="BSD 2-clause \"Simplified\" License",
-    packages=setuptools.find_packages(),
-    project_urls={
-        "documentation": "https://modypy.readthedocs.io/",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-    python_requires=">=3.6",
-    install_requires=[
-        "numpy>=1.18.1",
-        "scipy>=1.5.4",
-    ],
-    setup_requires=[
-        "pytest-runner",
-    ],
-    tests_require=[
-        "pytest",
-    ]
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
 )
